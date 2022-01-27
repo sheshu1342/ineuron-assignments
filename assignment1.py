d0343fdfb6936615f3efcd859dc9c0cf257f05c7
sheshu1342 <97656364+sheshu1342@users.noreply.github.com>
2022-01-27 18:49:23 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators.
+# 
+#     
+#     *  =Multiplication Expression
+# 'hello' = string
+# -87.8 = values _float
+# -  = Expression
+# /  = Expression
+# +	= Expression
+# 6  = value 
+# 
+# 
+# 2. What is the difference between string and variable?
+# String contains charecters which can be filled inside varibles . variables are the data holding (data storing).
+# 
+# 3. Describe three different data types.
+# String , integer , tuples, dictionary ,list
+# 
+# 4. What is an expression made up of? What do all expressions do?
+# Expression contains values and operators. It brings down it to single value or excecutes the expression
+# 
+# 
+# 5. This assignment statements, like spam = 10. What is the difference between an expression and a statement?
+# Expression is  to excuition of values and functions into single entity . where Statement it self a  single entity.
+# 
+# 
+# 6. After running the following code, what does the variable bacon contain?
+# bacon = 22
+# bacon + 1
+# ans : 22
+# 7. What should the values of the following two terms be?
+# 'spam' + 'spamspam'
+# ans : 'spamspamspam'
+# 
+# 
+# 'spam' * 3
+# ans : 'spamspamspam'
+# 
+# 8. Why is eggs a valid variable name while 100 is invalid?
+# Variable contains values/stores data, so numbers cant be a variable . mathematically inequality will arise like( 1 = 100) to avoid these also varibles starts with charecters.
+# 
+# 9. What three functions can be used to get the integer, floating-point number, or string version of a value?
+# Int() ,float() ,str()
+# 
+# 10. Why does this expression cause an error? How can you fix it?
+# 'I have eaten ' + 99 + ' burritos.'
+# 
+# ans :'I have eaten ' + ‘99’ + ' burritos.'
+# 
+
+# In[ ]:
+
+
+
+
