7e5b6d12ccb4480986309d784568338ab84049b0
sheshu1342 <97656364+sheshu1342@users.noreply.github.com>
2022-01-29 19:43:53 +0530

    Add files via upload

---
+++ assignment3.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#1. Why are functions advantageous to have in your programs?
+
+For repitative usage ,no need of  writing the code again and again . 
+its more productive by calling /using the functions .
+
+
+# In[ ]:
+
+
+#2. When does the code in a function run: when it's specified or when it's called?
+
+When its called
+
+
+# In[ ]:
+
+
+#3. What statement creates a function?
+
+def
+
+
+# In[ ]:
+
+
+#4.	What is the difference between a function and a function call?
+Function is a block of code which exsiccates when input is given.
+Functional call is like using functions to complete the desired task
+
+
+# In[ ]:
+
+
+#5.	How many global scopes are there in a Python program? How many local scopes?
+1 global & 1 local scope per  execution
+
+
+# In[ ]:
+
+
+#6.	What happens to variables in a local scope when the function call returns?
+The local variables will be destroyed
+
+
+# In[ ]:
+
+
+#7.	What is the concept of a return value? Is it possible to have a return value in an expression?
+It returns the value after completing task/function. possible to have a return value in an expression
+
+
+# In[ ]:
+
+
+#8.	If a function does not have a return statement, what is the return value of a call to that function?
+
+none
+
+
+# In[ ]:
+
+
+
+#9.	How do you make a function variable refer to the global variable?
+Using GLOBAL key , or specifying the variables outside the function
+
+
+# In[ ]:
+
+
+#10.	What is the data type of None?
+Null type
+
+
+# In[ ]:
+
+
+#11.	What does the sentence import areallyourpetsnamederic do?
+It imports a module (present word suggests no mmodule)
+
+
+# In[ ]:
+
+
+#12.	If you had a bacon() feature in a spam module, what would you call it after importing spam?
+Spam.bacon()
+
+
+#13.	What can you do to save a programme from crashing if it encounters an error?
+By using TRY & EXCEPTION BLOCK
+
+
+
+#14. What is the purpose of the try clause? What is the purpose of the except clause?
+Try block defines that block code is suspicious, to catch a error.
+Exception handles it without stopping/crashing the code, helps to proceed without throwing warning 
+
